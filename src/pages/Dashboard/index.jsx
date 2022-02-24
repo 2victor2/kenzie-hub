20e36620316136fb5a44cdb816aeb72133d55a2e
Victor Ramon <victorrgds02@gmail.com>
2022-02-23 22:32:09 -0300

    feat: change/create tech modals

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -5,14 +5,33 @@ import {
   Heading,
   HStack,
   Text,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  FormControl,
+  SimpleGrid,
+  GridItem,
 } from "@chakra-ui/react";
+import { useState } from "react";
 import Header from "../../components/Header";
+import Input from "../../components/Input";
 import List from "../../components/List";
+import Select from "../../components/Select";
 
 const Dashboard = () => {
+  const [modalType, setModalType] = useState("Alterar Tecnologia");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleModal = (type) => {
+    setModalType(type);
+  };
   return (
     <Container maxW="1440px" p={0}>
-      <Flex flexDir="column" pb={[5, 10]} >
+      <Flex flexDir="column" pb={[5, 10]}>
         <Header />
         <Flex
           h={["131px", "118px"]}
@@ -71,6 +90,64 @@ const Dashboard = () => {
           <List />
         </Flex>
       </Flex>
+      <Modal isCentered closeOnOverlayClick isOpen={true} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent w={["295px", "370px"]}>
+          <ModalHeader
+            color="brand.grey0"
+            bg="brand.grey2"
+            h={["40px", "50px"]}
+            px={["17px", "22px"]}
+          >
+            {modalType}
+          </ModalHeader>
+          <ModalCloseButton color="brand.grey1" top="var(--chakra-space-4)" />
+          <ModalBody bg="brand.grey3" px={["17px", "22px"]}>
+            <FormControl>
+              <Input
+                label={
+                  modalType === "Cadastrar Tecnologia"
+                    ? "Nome"
+                    : "Nome do projeto"
+                }
+                placeholder={
+                  modalType === "Cadastrar Tecnologia"
+                    ? "Digite o nome"
+                    : "Digite o novo nome"
+                }
+              />
+              <Select
+                label={
+                  modalType === "Cadastrar Tecnologia"
+                    ? "Selecione o status"
+                    : "Status"
+                }
+                placeholder="Iniciante"
+              >
+                <option value="Intermediário">Intermediário</option>
+                <option value="Avançado">Avançado</option>
+              </Select>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter bg="brand.grey3" px={["17px", "22px"]} pt={0}>
+              {modalType === "Cadastrar Tecnologia" ? (
+                <Button variant="primary" w="100%" minH={["38.38px", "48px"]} fontSize={["0.8rem", "1rem"]} >Cadastrar Tecnologia</Button>
+              ) : (
+                <SimpleGrid  w="100%" columns={3} gap={2}>
+                  <GridItem colSpan={2}>
+                    <Button variant="primary" minH={["38.38px", "48px"]} w="100%" fontSize={["0.8rem", "1rem"]} isDisabled>
+                      Salvar alterações
+                    </Button>
+                  </GridItem>
+                  <GridItem colSpan={1}>
+                    <Button variant="grey" w="100%" minH={["38.38px", "48px"]} fontSize={["0.8rem", "1rem"]} >Excluir</Button>
+                  </GridItem>
+            </SimpleGrid>
+              )}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Container>
   );
 };
--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -104,6 +104,7 @@ const theme = extendTheme({
         },
       },
     },
+    
   },
 });
 
